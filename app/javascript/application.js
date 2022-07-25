56229f30d4f3c0311a4c03859435a046f0fb26eb
Kate Romanycheva <esromanycheva@gmail.com>
2022-07-25 09:45:50 +0300

    lightbox2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import 'lightbox2'
 
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
   return new bootstrap.Popover(popoverTriggerEl)  
-})  
+})
