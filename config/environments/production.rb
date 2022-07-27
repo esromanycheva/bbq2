4e0ed1242f4ca9e0656e78bfa301d1153b62f157
esromanycheva <94757683+esromanycheva@users.noreply.github.com>
2022-07-27 20:45:08 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: Rails.application.credentials.dig(:gmail, :user_name),
+    password: Rails.application.credentials.dig(:gmail, :password),
+    authentication: 'plain'
+  }
 end
